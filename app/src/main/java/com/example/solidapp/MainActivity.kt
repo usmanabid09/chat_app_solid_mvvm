8750719fbb828a2f0a528844b2d6d128c548c7ad
Usman Abid <usmanabid1221@gmail.com>
2021-11-08 15:56:05 +0500

    Integrated Splash Screen

--- app/src/main/java/com/example/solidapp/MainActivity.kt
+++ app/src/main/java/com/example/solidapp/MainActivity.kt
@@ -1,13 +1,13 @@
 package com.example.solidapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+//        installSplashScreen()
+        setTheme(R.style.Theme_SolidApp)
         setContentView(R.layout.activity_main)
-
-        // Starting
     }
 }
